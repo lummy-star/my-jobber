57d0a21bfd867bf80b85a9e82f97daa710f13b07
abisalde <isaiah.abiodun@outlook.com>
2022-04-10 10:11:02 -0400

    first commit

--- errors/index.js
+++
@@ -1,5 +0,0 @@
-import NotFoundError from "./not-found.js";
-import BadRequestError from "./bad-request.js";
-import UnAuthenticatedError from "./UnAuthenticated.js";
-
-export {NotFoundError, BadRequestError, UnAuthenticatedError}
